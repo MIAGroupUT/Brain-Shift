16570ff436b7d1fe709d3494bf274d63d0ceb0ec
Baris Imre <barisimre98@gmail.com>
2024-01-28 22:23:30 +0100

    Make main better

--- src/main.py
+++ src/main.py
@@ -1,4 +1,13 @@
 from src.trainings.segmentation import train_segmentation
+import wandb
 
 if __name__ == '__main__':
-    train_segmentation(batch_size=16, run_name="Debug", location="/home/baris/Documents/brain-morphing")
+    run_name = "Debug"
+    # location = "/home/baris/Documents/brain-morphing"
+    location = "/home/imreb/brain-morphing"
+
+    wandb.init(project="Debug",
+               entity="barisimre",
+               name=run_name)
+
+    train_segmentation(batch_size=8, run_name=run_name, location=location)
