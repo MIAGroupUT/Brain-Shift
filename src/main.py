f72bc38b127eff858d13d81da7db94c18d354e24
b.imre-1@utwente.nl <imreb@hpc-head2.local>
2024-02-24 00:39:14 +0100

    Cluster

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
 
 
     # run_name = "seg_yesflip_norot_5000"
-    run_name = 'training_with_hdf5'
+    run_name = 'training_with_hdf5_just_in_case'
     # location = "/home/baris/Documents/brain-morphing"
     location = "/home/imreb/brain-morphing"
     # location = "/home/baris/Documents/work/brain-morphing"
@@ -19,7 +19,7 @@ if __name__ == '__main__':
                name=run_name,
                notes="Got some new annotations")
 
-    train_segmentation(batch_size=2, run_name=run_name, location=location, hdf5_name="new_full_annotations_centered.hdf5", dims=3, num_epochs=5000, loader_num_workers=8)
+    train_segmentation(batch_size=2, run_name=run_name, location=location, hdf5_name="annotated.hdf5", dims=3, num_epochs=5000, loader_num_workers=8)
     # optimize_centers(location=location, run_name=run_name, batch_size=1, num_epochs=90, hdf5_target="new_annotations.hdf5")
     # train_morph(run_name=run_name, num_epochs=500, location=location, data_location="/data/final_dataset.hdf5", num_workers=8, log=True, mode='aided')
     # train_morph_instant(run_name=run_name, num_epochs=150, location=location, data_location="/data/final_dataset.hdf5", num_workers=8, log=True, mode='instance_aided', lr=9e-4)
